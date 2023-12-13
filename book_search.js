be8cac6b43d763cf244a45b48c4d92193679e482
Joseph A. Esquivel <85577762+JosephEsquivel1@users.noreply.github.com>
2023-12-12 19:39:31 -0600

    Update book_search.js

--- book_search.js
+++ book_search.js
@@ -2,9 +2,7 @@
  * book_search.js
  * 
  *  Implementation of the function: findSearchTermInBooks().
- *  For USDC 2023
- *
- *  By Joseph Esquivel 
+ *  By Joseph Esquivel
  *  
  * 
  * 
@@ -17,18 +15,20 @@
  * @returns {JSON} - Search results.
  * */ 
  function findSearchTermInBooks(searchTerm, scannedTextObj) {
+     // Create result object, returned in current condition if no results found.
     var result = {
         "SearchTerm": searchTerm,
         "Results": []
     };
-
+    // Iterate through each book 
     for (const book of scannedTextObj) {
-        const {ISBN, Content } = book;
-        
+        const { ISBN, Content } = book;
+        // Iterate through each line of each page
         for (const line of Content) {
             const { Page, Line, Text } = line;
             
             if (Text.includes(searchTerm)) {
+                // Update result object for later return
                 result.Results.push({
                     "ISBN": ISBN,
                     "Page": Page,
@@ -77,6 +77,46 @@ const twentyLeaguesOut = {
     ]
 }
 
+
+const test3ExpectedOutput = {
+    "SearchTerm": "simply",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+};
+
+
+const test4ExpectedOutput = {
+    "SearchTerm": "and",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        },
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 10
+        }
+    ]
+};
+
+const test5ExpectedOutput = {
+    "SearchTerm": "The",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+};
+
 /*
  _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
 | | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
@@ -103,6 +143,7 @@ if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
     console.log("Received:", test1result);
 }
 
+
 /** We could choose to check that we get the right number of results. */
 const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
 if (test2result.Results.length == 1) {
@@ -112,5 +153,48 @@ if (test2result.Results.length == 1) {
     console.log("Expected:", twentyLeaguesOut.Results.length);
     console.log("Received:", test2result.Results.length);
 }
+
+// Positive test, should return a match:
+const test3Result = findSearchTermInBooks("simply", twentyLeaguesIn);
+if (JSON.stringify(test3ExpectedOutput) === JSON.stringify(test3Result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", test3ExpectedOutput);
+    console.log("Received:", test3Result);
+}
+
+// Test Result Sensitivity, "darkn-ness":
+const test4Result = findSearchTermInBooks("and", twentyLeaguesIn);
+if (JSON.stringify(test4ExpectedOutput) === JSON.stringify(test4Result)) {
+    console.log("PASS: Test 4");
+} else {
+    console.log("FAIL: Test 4");
+    console.log("Expected:", test4ExpectedOutput);
+    console.log("Received:", test4Result);
+}
+
+
+// Test Character Sensitivity, "the" vs "The":
+const test5result = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(test5ExpectedOutput) === JSON.stringify(test5result)) {
+    console.log("PASS: Test 5");
+} else {
+    console.log("FAIL: Test 5");
+    console.log("Expected:", twentyLeaguesOut);
+    console.log("Received:", test5result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
